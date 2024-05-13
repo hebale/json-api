389a57686c9579c2ec9b150c4fe6e8dbd149b20c
Jong Kyu, Yun <40757693+hebale@users.noreply.github.com>
2024-05-13 09:27:01 +0900

    Delete public directory

--- public/components/CreateForm.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
-import { FormGroup, FormControl, OutlinedInput, InputLabel, } from "@mui/material";
-import CodeEditor from "~/components/CodeEditor";
-var CreateForm = function () {
-    return (_jsxs(_Fragment, { children: [_jsxs(FormGroup, { children: [_jsxs(FormControl, { size: "small", children: [_jsx(InputLabel, { children: "path" }), _jsx(OutlinedInput, { label: "path", value: "/user/info" })] }), _jsxs(FormControl, { size: "small", children: [_jsx(InputLabel, { children: "method" }), _jsx(OutlinedInput, { label: "method" })] })] }), _jsx(CodeEditor, { data: "{\n          \"name\": \"\uC724\uC885\uADDC\",\n          \"age\": 32,\n          \"data\": false \n        }" })] }));
-};
-export default CreateForm;
--- public/layout/Body.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Box } from "@mui/material";
-import CreateForm from "~/components/CreateForm";
-var Body = function () {
-    return (_jsx(Box, { children: _jsx(CreateForm, {}) }));
-};
-export default Body;
